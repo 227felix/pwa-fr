fcc02975b2f398320e6b2140be4fa2f932f9435a
227felix <felix.grenzing@gmail.com>
2024-05-01 14:49:07 +0200

    brought back up 2

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from helpers import login_required, apology
+from functools import wraps
+
+#from helpers import login_required, apology
+
+## helpers.py
+def login_required(f):
+    """
+    Decorate routes to require login.
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+# end of helpers.py
+
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
@@ -27,3 +64,5 @@ def login():
         flash('You were successfully logged in')
         return redirect('/')
     return render_template('login.html')
+
+
