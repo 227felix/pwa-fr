d156f5aee7c5dbad451f099e1be63a10b51fa572
227felix <felix.grenzing@gmail.com>
2024-05-02 16:04:05 +0200

    fixes all around

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-      caches.open('your-cache-name').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/haupt-css.css',
-          '/haupt-javascript.js'
-        ]);
-      })
+        caches.open('your-cache-name').then(function (cache) {
+            console.log('[Service Worker] Pre-caching App Shell');
+            return cache.addAll([
+                '/',
+                '/login'
+            ]);
+        })
     );
-  });
-
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
 });
-  
-  self.addEventListener('fetch', function(event) {
+
+self.addEventListener('activate', function (event) {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+});
+
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
     );
-  });
+});
