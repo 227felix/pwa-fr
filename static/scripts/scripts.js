f8157e8d460f8444f57ad54c66a85453bc4e2e32
227felix <felix.grenzing@gmail.com>
2024-05-04 18:42:37 +0200

    added darkmode comp fix6

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,29 +1,8 @@
-function setThemeBasedOnPrefersColorScheme() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.body.classList.add('dark-theme');
-        console.log('Dark theme detected');
-    } else {
-        document.body.classList.remove('dark-theme');
-    }
-}
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    setThemeBasedOnPrefersColorScheme();
 
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-        const newColorScheme = e.matches ? "dark" : "light";
-        if (newColorScheme === "dark") {
-            document.body.classList.add('dark-theme');
-        } else {
-            document.body.classList.remove('dark-theme');
-        }
-    });
 });
 
-document.addEventListener('visibilitychange', () => {
-    if (!document.hidden) {
-        setThemeBasedOnPrefersColorScheme();
-    }
-});
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
