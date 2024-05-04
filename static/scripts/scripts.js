263d715ad7074a23fdb314c601a0a2e640d32797
227felix <felix.grenzing@gmail.com>
2024-05-04 18:28:34 +0200

    added darkmode comp fix4

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('dark-theme');
+        console.log('Dark theme detected');
+    }
     // Force dark theme
-    document.body.classList.add('dark-theme');
-    console.log('DOM fully loaded and parsed');
 });
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js').then(function (registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
